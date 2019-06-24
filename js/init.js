a858aaed6a8b12ae243bc6a4d96752db6bb7527a
Mtperes <macintosh@MBPdeMacintosh.home>
2019-06-24 22:22:52 +0100

    Content update Complete
    Now, add up features will be the main focus

--- js/init.js
+++ js/init.js
@@ -9,7 +9,7 @@
     console.log('hello world');
     console.log('Welcome to the Back End');
 
-    $('.scrollspy').scrollSpy( 100);
+    $('.scrollspy').scrollSpy({target: ".academic", offset: -900});
   
     
     var delay = 1000;    
@@ -30,96 +30,96 @@
       $('#btn-back').delay( delay ).fadeIn(delay);
        console.log("fade in back" );$('#btn-more').removeClass(" pulse");
 
-     });
+    });
 
-         
-// ——————————————————————————————————————————————————
-// TextScramble
-// ——————————————————————————————————————————————————
+          
+  // ——————————————————————————————————————————————————
+  // TextScramble
+  // ——————————————————————————————————————————————————
 
-class TextScramble {
-  constructor(el) {
-    this.el = el;
-    this.chars = "!<>-_\\/[&]{}@—=+?()";
-    this.update = this.update.bind(this);
-  }
-  setText(newText) {
-    const oldText = this.el.innerText;
-    const length = Math.max(oldText.length, newText.length);
-    const promise = new Promise(resolve => (this.resolve = resolve));
-    this.queue = [];
-    for (let i = 0; i < length; i++) {
-      const from = oldText[i] || "";
-      const to = newText[i] || "";
-      const start = Math.floor(Math.random() * 40);
-      const end = start + Math.floor(Math.random() * 40);
-      this.queue.push({ from, to, start, end });
+  class TextScramble {
+    constructor(el) {
+      this.el = el;
+      this.chars = "!<>-_\\/[&]{}@—=+?()";
+      this.update = this.update.bind(this);
     }
-    cancelAnimationFrame(this.frameRequest);
-    this.frame = 0;
-    this.update();
-    return promise;
-  }
-  update() {
-    let output = "";
-    let complete = 0;
-    for (let i = 0, n = this.queue.length; i < n; i++) {
-      let { from, to, start, end, char } = this.queue[i];
-      if (this.frame >= end) {
-        complete++;
-        output += to;
-      } else if (this.frame >= start) {
-        if (!char || Math.random() < 0.28) {
-          char = this.randomChar();
-          this.queue[i].char = char;
+    setText(newText) {
+      const oldText = this.el.innerText;
+      const length = Math.max(oldText.length, newText.length);
+      const promise = new Promise(resolve => (this.resolve = resolve));
+      this.queue = [];
+      for (let i = 0; i < length; i++) {
+        const from = oldText[i] || "";
+        const to = newText[i] || "";
+        const start = Math.floor(Math.random() * 40);
+        const end = start + Math.floor(Math.random() * 40);
+        this.queue.push({ from, to, start, end });
+      }
+      cancelAnimationFrame(this.frameRequest);
+      this.frame = 0;
+      this.update();
+      return promise;
+    }
+    update() {
+      let output = "";
+      let complete = 0;
+      for (let i = 0, n = this.queue.length; i < n; i++) {
+        let { from, to, start, end, char } = this.queue[i];
+        if (this.frame >= end) {
+          complete++;
+          output += to;
+        } else if (this.frame >= start) {
+          if (!char || Math.random() < 0.28) {
+            char = this.randomChar();
+            this.queue[i].char = char;
+          }
+          output += `<span class="dud">${char}</span>`;
+        } else {
+          output += from;
         }
-        output += `<span class="dud">${char}</span>`;
+      }
+      this.el.innerHTML = output;
+      if (complete === this.queue.length) {
+        this.resolve();
       } else {
-        output += from;
+        this.frameRequest = requestAnimationFrame(this.update);
+        this.frame++;
       }
     }
-    this.el.innerHTML = output;
-    if (complete === this.queue.length) {
-      this.resolve();
-    } else {
-      this.frameRequest = requestAnimationFrame(this.update);
-      this.frame++;
+    randomChar() {
+      return this.chars[Math.floor(Math.random() * this.chars.length)];
     }
   }
-  randomChar() {
-    return this.chars[Math.floor(Math.random() * this.chars.length)];
-  }
-}
 
-// ——————————————————————————————————————————————————
-// Example
-// ——————————————————————————————————————————————————
+  // ——————————————————————————————————————————————————
+  // Example
+  // ——————————————————————————————————————————————————
 
-const phrases = [
-  "Systems Developer",
-  "UX Designer",
-  "Digital Artist",
-  "FilmMaker",
-];
+  const phrases = [
+    "Systems Developer",
+    "UX Designer",
+    "Digital Artist",
+    "FilmMaker",
+  ];
 
-const el = document.querySelector("#mainTitle");
-const fx = new TextScramble(el);
+  const el = document.querySelector("#mainTitle");
+  const fx = new TextScramble(el);
 
-let counter = 0;
-const next = () => {
-  fx.setText(phrases[counter]).then(() => {
-    setTimeout(next, 1500);
+  let counter = 0;
+  const next = () => {
+    fx.setText(phrases[counter]).then(() => {
+      setTimeout(next, 1500);
+    });
+    counter = (counter + 1) % phrases.length;
+  };
+
+  next();
+
+
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
   });
-  counter = (counter + 1) % phrases.length;
-};
-
-next();
-
-
-$(document).ready(function(){
-  $('.collapsible').collapsible();
-});
- 
+  
   });   // end of document ready
 
 })(jQuery); // end of jQuery name space
